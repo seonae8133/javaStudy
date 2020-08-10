e1d5fde56a6b952ab5e160da3bbb1d026c5a684f
seonae <seonae@kakao.com>
2020-08-10 12:15:06 +0900

    seceond commit

---
+++ VariableExample.java
@@ -0,0 +1,16 @@
+public class VariableExample {
+	
+	public static void main (String[] args){
+		int value1 = 10;
+		int value2 = 20;
+		int result = value1+value2;
+		System.out.println("10 + 20 = " + result);
+		
+	}
+}
+
+
+/*main : main 메소드를 처리를 하겠다.
+  (String[] args) : 매개변수
+   int value1=10 - value1이라는 정수형 변수에 10을 담겠다
+*/
