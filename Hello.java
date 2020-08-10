27abfa8f1e785d26b5b0acb211231d8bd01e3b37
seonae <seonae@kakao.com>
2020-08-10 11:08:57 +0900

    first commit

---
+++ Hello.java
@@ -0,0 +1,8 @@
+public class Hello {
+	public static void main(String[] args) {
+		System.out.println("Hello, welcome to the java world!");
+		System.out.println("nice meet you!");
+		int myAge = 28;
+		System.out.println("My Age is" + myAge);
+	}
+}
