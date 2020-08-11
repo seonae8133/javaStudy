5b3c394b098b47ce8823b483abf93c28c0ae993f
seonae <seonae@kakao.com>
2020-08-11 09:10:25 +0900

    Example2

---
+++ VariableExample2.java
@@ -0,0 +1,11 @@
+public class VariableExample2 {
+	public static void main (String[] args){
+		
+	int n1=10;
+	int n2=20;, n3=30;
+	System.out.println("15+25=" + (15+25));
+	System.out.println("25+35=" + (25+35));
+	System.out.println("30+10=" + (30+10));
+	
+	}
+}
